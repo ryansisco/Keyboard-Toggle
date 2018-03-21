400af1506c175678c0e1e695be08c62e8955a362
Dent <ryansisco8@gmail.com>
2018-03-21 12:52:17 -0700

    added -help, fixed arg check

--- keyboard.c
+++ keyboard.c
@@ -3,8 +3,10 @@
 #include <unistd.h>
 #include <string.h>
 
+#define KEYBOARDNAME "AT Translated Set 2 keyboard"
+
 int main(int argc, char* argv[]) {
-	if (argc > 2) {
+	if (argc != 2) {
 		printf("Error: Usage ->     keyboard [option]\n");
 		exit(1);
 	}
@@ -14,13 +16,21 @@ int main(int argc, char* argv[]) {
 	if ((strcmp(argv[1], "off") == 0) || (strcmp(argv[1], "OFF") == 0)){
 		strcpy(argv[1], "0");
 	}
+	if ((strcmp(argv[1], "-help")) == 0) {
+		printf("Usage ->	keyboard [option]\n");
+		printf("Options: 0, 1, on, off\n");
+		system("xinput -list");
+		printf("Default Keyboard name: AT Translated Set 2 keyboard\n");
+		printf("Change this in the 6th line of the code & compile\n");
+		exit(0);
+	}
 	char data[2000];
 	char keyboard_id[4];
 	FILE *fp;
 	int i;
 	fp = popen("xinput -list", "r");
 	while (fgets(data, 2000, fp) != NULL) {
-		if (strstr(data, "AT Translated Set 2 keyboard")) {
+		if (strstr(data, KEYBOARDNAME)) {
 			for (i = 0; i < strlen(data); i++) {
 				if (data[i] == '=') {
 					i++;
