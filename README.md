5d61e2058e0bab4e049ae0e561545e9480b1253a
Dent <ryansisco8@gmail.com>
2018-04-22 00:00:34 -0700

    completed installer

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
 # Keyboard-Toggle
-Turns on/off built in laptop keyboard for linux. 
+Turns on/off built in laptop keyboard for Ubuntu. 
+
+run "make" to compile
+run "./keyboard -help" for help
+run "./keyboard -change" to change settings if needed
+run "./keyboard [on, ON, 1]" to turn it on
+run "./keyboard [off, OFF, 0]" to turn it off
+
 
 If you have locked your keyboard, and cannot
 connect again, restart your laptop.
-
-This was tested and designed for my Ubuntu XPS13.
-It should work on other distros/models, but who knows.
-
-keyboard -help for help
-
-Turn OFF keyboard: keyboard off, keyboard OFF, keyboard 0
-  
-Turn ON keyboard:keyboard on, keyboard ON, keyboard 1
--- userdevchange.c
+++ userdevchange.c
@@ -6,28 +6,35 @@
 void editkeyboard(char change[2000]) {
 	char choice[2000];
 	char data[2000];
-	printf("New device will be: %s\n", change);
+	printf("New device will be: %s", change);
 	printf("Are you sure??? (y/n): ");
-	scanf("%s", choice);
+	fgets(choice, 2000, stdin);
 	if ((choice[0] != 'y') && (choice[0] != 'Y')) {
 		printf("Cancelling...\n");
 		exit(0);
 	}
 	char swap[2000] = "#define KEYBOARDNAME \""; 
 	strcat(swap, change);
-	strcat(swap, "\"");
+	swap[strlen(swap)-1] = '\"';
 	FILE *fp;
-	fp = popen("keyboard.c", "w+");
+	FILE *fp2;
+	fp = fopen("keyboard.c", "r");
+	fp2 = fopen("tempfile.c", "w+");
 	while (fgets(data, 2000, fp) != NULL) {
 		if (strstr(data, "#define")) {
-			fprintf(fp, "%s", swap);
+			fprintf(fp2, "%s\n", swap);
 		}
 		else {
-			fprintf(fp, "%s", data);
+			fprintf(fp2, "%s", data);
 		}
 	}
-	pclose(fp);
+	fclose(fp);
+	fclose(fp2);
+	sleep(1);
+	system("mv tempfile.c keyboard.c");
+	sleep(1);
 	system("gcc keyboard.c -o keyboard");
+	printf("Changed!\n");
 }
 
 int main(int argc, char* argv[]) {
@@ -41,13 +48,13 @@ int main(int argc, char* argv[]) {
 		i++;
 	}
 	printf("\nIs this correct? (y/n): ");
-	scanf("%s", choice);
+	fgets(choice, 2000, stdin);
 	if ((choice[0] == 'y') || (choice[0] == 'Y')) {
 		exit(0);
 	}
 	if ((choice[0] == 'n') || (choice[0] == 'N')) {
 		printf("New Device Name: ");
-		scanf("%s", change);
+		fgets(change, 2000, stdin);
 	}
 	else {
 		printf("Bad input, exiting...\n");
